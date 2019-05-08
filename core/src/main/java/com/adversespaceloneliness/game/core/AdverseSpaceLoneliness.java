b327dce3b537e4da133fe5d6e0533f6e52842ece
Benoît Dubreuil <b.dubreuil@hotmail.com>
2019-05-08 10:51:59 -0400

    Modified the desktop main so that it uses the texture atlas

--- core/src/main/java/com/adversespaceloneliness/game/core/AdverseSpaceLoneliness.java
+++ core/src/main/java/com/adversespaceloneliness/game/core/AdverseSpaceLoneliness.java
@@ -4,25 +4,38 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class AdverseSpaceLoneliness extends ApplicationAdapter {
 
     SpriteBatch batch;
     Texture img;
+    TextureAtlas atlas;
+    Sprite sprite;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         img = new Texture("assets/generated/texture/badlogic.jpg");
+
+
+        atlas = new TextureAtlas(Gdx.files.internal("assets/generated/sprite/spritesheet.atlas"));
+        sprite = atlas.createSprite("retro-space-pixel-shooter-pack/misc/nuke", 5);
+        sprite.setPosition(400, 400);
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
+
         batch.draw(img, 0, 0);
+        sprite.draw(batch);
+
         batch.end();
     }
 
@@ -30,5 +43,6 @@ public class AdverseSpaceLoneliness extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         img.dispose();
+        atlas.dispose();
     }
 }
