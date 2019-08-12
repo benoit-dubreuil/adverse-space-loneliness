5fe02dd10e8461c0f4f1c6b69305b5c35f41c14b
Benoit Dubreuil <b.dubreuil@hotmail.com>
2019-08-11 20:18:03 -0400

    Improved the AssetType enum. Added the ICachedEnumValues interface.

--- assets/src/main/java/com/adversespaceloneliness/game/assets/api/AssetType.java
+++ assets/src/main/java/com/adversespaceloneliness/game/assets/api/AssetType.java
@@ -1,13 +1,12 @@
 package com.adversespaceloneliness.game.assets.api;
 
-import java.util.Arrays;
-
 /**
  * All asset types in the LibGDX game engine and their supported extensions.
  * <p>
- * All asset types in this enum store their extensions twice : firstly as plain extensions and secondly as extension prefixed with a dot.
+ * All asset types in this enum store their extensions as plain extensions without a dot prefix.
  */
-public enum AssetType {
+public enum AssetType implements ICachedEnumValues<AssetType> {
+
     IMAGE(new String[] { "jpg", "jpeg", "png", "bmp" }),
     AUDIO(new String[] { "wav", "mp3", "ogg" }),
     FONT(new String[] { "fnt", "ttf" }),
@@ -16,19 +15,55 @@ public enum AssetType {
     LOCALIZATION(new String[] { "properties" }),
     PARTICLE_EFFECT(new String[] { "p" });
 
+    public static final AssetType[] cachedEnumValues = AssetType.values();
+
+    private static final char ID_SUFFIX_SEPARATOR = '_';
+
     private final String[] m_filenameExtensions;
-    private final String[] m_filenameExtensionsWithDot;
 
     AssetType(String[] filenameExtensions) {
         m_filenameExtensions = filenameExtensions;
-        m_filenameExtensionsWithDot = (String[]) Arrays.stream(filenameExtensions).map(extension -> '.' + extension).toArray();
+    }
+
+    /**
+     * Gets the {@link AssetType} correspond to the supplied extension.
+     * <p>
+     * The extension must not contain a dot prefix.
+     *
+     * @param soughtExtension The file name extension without a dot prefix.
+     *
+     * @return Returns the correspond <code>enum</code> value to the extension.
+     */
+    public static AssetType getAssetTypeFromExtension(String soughtExtension) {
+
+        for (AssetType assetType : cachedEnumValues) {
+            for (String assetTypeExtension : assetType.getFilenameExtensions()) {
+                if (soughtExtension.equals(assetTypeExtension)) {
+                    return assetType;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public final AssetType[] getCachedEnumValues() {
+        return cachedEnumValues;
+    }
+
+    /**
+     * Computes the suffix for asset IDs for code generation.
+     * <p>
+     * The suffix is the ID suffix separator concatenated with the asset type.
+     *
+     * @return The suffix for asset IDs for code generation
+     */
+    public String computeIDSuffix() {
+        return ID_SUFFIX_SEPARATOR + name();
     }
 
     public String[] getFilenameExtensions() {
         return m_filenameExtensions;
-    }
-
-    public String[] getFilenameExtensionsWithDot() {
-        return m_filenameExtensionsWithDot;
     }
 }
